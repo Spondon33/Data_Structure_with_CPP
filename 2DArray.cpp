151d72754689128e386149a64a76a5ab8f19850e
Spondon33 <nurulaminkh33@gmail.com>
2025-10-09 23:39:30 +0600

    ...

--- 2DArray.cpp
+++ 2DArray.cpp
@@ -22,4 +22,31 @@ int main(){
         cout << endl;
     }
     cout << endl;
+
+    //Transpose Array
+    cout << "Transpose Array: " << endl;
+    for (int j = 0; j < col; j++) {
+        for (int i = 0; i < row; i++) {
+            cout << Arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    //Searching an element
+    int search;
+    cout << "Enter the element you want to search: " << endl;
+    cin >> search;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            if (Arr[i][j] == search){
+                cout << search << " found at index [" << i << "] [" << j << "]";
+                return 1;
+            }
+        }
+        cout << endl;
+    }
+    cout << "Element not found";
+
+    return 0;
 }
