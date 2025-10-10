7c721ec1543f66dd264813c0e1658f7fa103b532
Spondon33 <nurulaminkh33@gmail.com>
2025-10-10 23:14:19 +0600

    ...

--- string.cpp
+++ string.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
@@ -13,7 +14,25 @@ int main(){
 
     cout << endl;
     cout << "Accessing String without using array or loop: " << endl;
-    cout << s;
+    cout << s << endl;
+
+    //String functions
+    string str = "Hello";
+
+    cout << "Length of the string: " << str.length() << endl; // using .length()
+    cout << "Char at index 1: " << str.at(1) << endl; // using .at()
+    cout << "Char at index 2: " << str[2] << endl; // using []
+
+    str.append(" World!"); // appends string
+    cout << "Appended Text: " << str << endl;
+
+    str.insert(5, ";"); // inserts ; at index 5
+    cout << "After Insertion: " << str << endl;
+
+    str.erase(5,1); // erases 1 char from index 5
+    cout << "After erase: " << str << endl;
+
+    cout << str.find("lo") << endl; // finds the index of the substring
 
     return 0;
 }
