8f9d819b9ae4077963a0f58beed3281b5625700e
Spondon33 <nurulaminkh33@gmail.com>
2025-10-16 15:23:53 +0600

    ...

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,3 +1,37 @@
-//
-// Created by USER on 10/8/2025.
-//
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int Esize, int search){
+    int left = 1;
+    int right =  Esize;
+
+    while(left <= right){
+        int mid = (left + right) / 2;
+        if (arr[mid] = search){
+            return mid;
+        }
+        else if (arr[mid] < search){
+            left = mid + 1;
+        }
+        else{
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[10] = {1, 2, 3, 4, 5, 6};
+    int Esize = 6;
+    int search = 3;
+    cout << endl;
+    int result = binarySearch(arr, Esize, search);
+    if (result == -1){
+        cout << "Element " << search << " is not found" << endl;
+    }
+    else{
+        cout << "Element " << search << " is found in index " << result << endl;
+    }
+
+    return 0;
+}
